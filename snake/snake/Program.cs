7a9f8ed0881edd9e3ba173d61016970da2640dee
tadam <zakharov.vn92@gmail.com>
2021-09-11 19:36:11 +0300

    'My_First_Game_Snake'

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -48,7 +48,25 @@ namespace snake
 					snake.HandleKey(key.Key);
 				}
 			}
+			WriteGameOver();
+			Console.ReadLine();
 		}
 
+		static void WriteGameOver()
+		{
+			int xOffset = 25;
+			int yOffset = 8;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.SetCursorPosition(xOffset, yOffset++);
+			WriteText("============================", xOffset, yOffset++);
+			WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
+			WriteText("============================", xOffset, yOffset++);
+		}
+
+		static void WriteText(String text, int xOffset, int yOffset)
+		{
+			Console.SetCursorPosition(xOffset, yOffset);
+			Console.WriteLine(text);
+		}
 	}
 }
