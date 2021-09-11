23f904fde90168f5433cdd96537c2c4fe187cdab
tadam <zakharov.vn92@gmail.com>
2021-09-11 19:27:21 +0300

    'Lesson-14'

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -6,45 +6,49 @@ using System.Threading;
 
 namespace snake
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            
-            // рамка
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '#');
-            HorizontalLine downline = new HorizontalLine(0, 78, 24, '#');
-            VerticalLine leftline = new VerticalLine(0, 24, 0, '#');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '#');
-            upLine.Draw();
-            downline.Draw();
-            leftline.Draw();
-            rightLine.Draw();
 
-            // рамка
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.SetBufferSize(80, 25);
 
-           
-            Point p = new Point(4, 5, '#');            
-            Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Draw();
+			Walls walls = new Walls(80, 25);
+			walls.Draw();
 
-            while(true)
-            {
-                if(Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey();
-                    snake.HandleKey(key.Key);
-                        
-                }
-                Thread.Sleep(100);
-                snake.Move();
-            }
-            
+			// Отрисовка точек			
+			Point p = new Point(4, 5, '*');
+			Snake snake = new Snake(p, 4, Direction.RIGHT);
+			snake.Draw();
 
+			FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+			Point food = foodCreator.CreateFood();
+			food.Draw();
 
+			while (true)
+			{
+				if (walls.IsHit(snake) || snake.IsHitTail())
+				{
+					break;
+				}
+				if (snake.Eat(food))
+				{
+					food = foodCreator.CreateFood();
+					food.Draw();
+				}
+				else
+				{
+					snake.Move();
+				}
 
-        }
-        
-    
-    }
+				Thread.Sleep(100);
+				if (Console.KeyAvailable)
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+					snake.HandleKey(key.Key);
+				}
+			}
+		}
+
+	}
 }
